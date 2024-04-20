59fa3f94d04337247a3320c6e7ea5b5fd7288f67
HaoWei00 <choco.pancakeslol@gmail.com>
2024-04-21 02:33:25 +0800

    Key changes - Added validator to /query api - Added error handler for /query api - refactor code and added comments for explanation - minor changes on html formatting

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from typing import Annotated,List
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse, JSONResponse
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.core import StorageContext, load_index_from_storage
-from IPython.display import Markdown, display
-import os
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader,StorageContext, load_index_from_storage
+from IPython.display import Markdown
 from dotenv import load_dotenv
+import os
 
 #os.environ["OPENAI_API_KEY"]= "your API Key"
 
 #Get api from external file to avoid accidentally pushing api key
+#Comment out this part of the code if you wish to input your API key directly in python, else replace the path to your api key env
 config_path = "C:\\Users\\User\\Documents\\openai_key.env"
 load_dotenv(dotenv_path=config_path)
 api_key = os.getenv('OPENAI_API_KEY')
 os.environ["OPENAI_API_KEY"]= api_key
 
-app = FastAPI()
 
 #Ensure the data directory exists
 os.makedirs('data', exist_ok=True)
@@ -37,14 +36,19 @@ else:
 #initialize query engine
 query_engine = index.as_query_engine()
 
-#define function to update query engine
+#define function to update query engine from api calls to ensure real-time update
 def update_query_engine(index):
     global query_engine
     query_engine = index.as_query_engine()
 
+
+app = FastAPI()
+
 # Function to filter out all non txt files
 def filter_file_format(files: List[UploadFile]) -> List[UploadFile]:
     filtered_files = [file for file in files if file.filename.endswith('.txt')]
+
+    #Record removed files for response
     removed_files = [file for file in files if not file.filename.endswith('.txt')]
     removed_documents=[]
     for removed_file in removed_files:
@@ -52,6 +56,7 @@ def filter_file_format(files: List[UploadFile]) -> List[UploadFile]:
 
     return removed_documents,filtered_files
 
+#Ingest API Definition
 @app.post("/ingest")
 async def ingest(
     files: Annotated[
@@ -65,46 +70,48 @@ async def ingest(
     #filter out non txt files 
     removed_documents,files=filter_file_format(files)
 
-
+    # Variable to save filenames for output
     new_documents = []
-    #Loop through files uploaded
-    for file in files:
-        out_file_path = os.path.join('data', file.filename)
-        try:
-            with open(out_file_path, 'wb') as out_file:
-                while True:
-                  #Read in chunks of 1MB
-                    chunk = await file.read(1024*1024)
-                    if not chunk:
-                        break
-                    out_file.write(chunk)
-            new_documents.append(out_file_path)
 
-        except Exception as e:
-            return JSONResponse(status_code=500, content={"message": f"Failed to process file {file.filename}: {str(e)}"})
+    # Add files into data folder, and update query engine if there's any txt file uploaded
+    if files:
+      for file in files:
+          out_file_path = os.path.join('data', file.filename)
+          try:
+              with open(out_file_path, 'wb') as out_file:
+                  while True:
+                    #Read in chunks of 1MB
+                      chunk = await file.read(1024*1024)
+                      if not chunk:
+                          break
+                      out_file.write(chunk)
+              new_documents.append(out_file_path)
+          except Exception as e:
+              return JSONResponse(status_code=500, content={"message": f"Failed to process file {file.filename}: {str(e)}"})
 
-
-
-    # Update the index with new documents, reload query engine
-    if new_documents:
-        documents = SimpleDirectoryReader("data").load_data()
-        index = VectorStoreIndex.from_documents(documents)
-        index.storage_context.persist()
-        update_query_engine(index)
-        
-    
-            
+      # Update the index with new documents, reload query engine
+      documents = SimpleDirectoryReader("data").load_data()
+      index = VectorStoreIndex.from_documents(documents)
+      index.storage_context.persist()
+      update_query_engine(index)
+              
     filenames = [file.filename for file in files]
     return {"message": f"Removed from uploads (Non-txt format):<br>- {'<br>- '.join([os.path.basename(doc) for doc in removed_documents])}<br><br>Successfully uploaded and saved:<br>- {'<br>- '.join([os.path.basename(doc) for doc in new_documents])}<br><br>"}
 
+#Query API Definition
 @app.get("/query")
 async def search_query(query: str ):
     #Retrieve response from the query engine
-    response =  query_engine.query(query)
-    results = Markdown(f"<b>{response}</b>")
-    return {"query": query, "results": results.data}
+    if query =='':
+        return JSONResponse(status_code=400, content={"message": "No query text detected. Please ensure query is not empty."}) 
+    try: 
+      response =  query_engine.query(query)
+      results = Markdown(f"<b>{response}</b>")
+      return {"query": query, "results": results.data}
+    except Exception as e:
+      return JSONResponse(status_code=500, content={"message": f"Failed to process query: {str(e)}"})
 
-
+#Main interface for web application
 @app.get("/")
 async def main():
     content = """
@@ -115,6 +122,7 @@ async def main():
     <button type="submit">Upload Files</button>
   </form>
   <div id="upload-response"></div>
+  <br>
   <form id="search-form" method="get">
     <input name="query" type="text" placeholder="Enter your search query here...">
     <button type="submit">Search</button>
